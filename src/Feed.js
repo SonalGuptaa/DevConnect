01b7e2c74cf4d77bc4373b7d7aa897247f6dbeba
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2023-07-27 11:27:48 +0530

    servertimestamp

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState } from 'react'
+import { useEffect } from 'react';
 import { Avatar } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
@@ -7,8 +8,12 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import './css/feed.css'
 import Post from './Post';
 import db from "./firebasee";
-import { doc, collection, setDoc } from "firebase/firestore";
+import { doc, collection, setDoc, } from "firebase/firestore";
+import {serverTimestamp } from "firebase/firestore";
+
+
 function Feed () { 
+  const [posts,setPost] = useState([]);
   const [input,setInput] = useState();
   async function submitPost(e){
     e.preventDefault();
@@ -20,9 +25,22 @@ function Feed () {
       message: input,
       photoURL:
         "https://tse2.mm.bing.net/th?id=OIP.jryuUgIHWL-1FVD2ww8oWgHaHa&pid=Api&P=0&h=180",
+        timestamp: serverTimestamp(),
     });
     setInput("");
   }
+  // useEffect tells react tha component needs to do something after render
+  // Its gets the data of post from firebase database
+  // useEffect(() => {
+  //   //  db.collection("post").orderBy("timestamp","desc").onSnapshot(snapshot=>{
+  //     collection(db,"post").orderBy("timestamp","desc").onSnapshot(snapshot=>{
+  //     setPost(snapshot.docs.map(doc=>({
+  //         id:doc.id,
+  //         data:doc.data()
+  //     })))
+  //    })
+  // },[])
+  // console.log(posts); 
   return (
     <div className='feed'>
         <div className='feed_input'>
